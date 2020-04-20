1d979e09641739fe43a0f8c8d990f151ac732912
nibuiro <31338759+nibuiro@users.noreply.github.com>
2020-04-20 11:22:48 +0900

    Update condconv.py

--- condconv/condconv.py
+++ condconv/condconv.py
@@ -10,7 +10,7 @@ from torch.nn.parameter import Parameter
 
 class _routing(nn.Module):
 
-    def __init__(self, in_channels, num_experts, dropout_rate=0.2):
+    def __init__(self, in_channels, num_experts, dropout_rate):
         super(_routing, self).__init__()
         
         self.dropout = nn.Dropout(dropout_rate)
@@ -71,7 +71,7 @@ class CondConv2D(_ConvNd):
 
     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
                  padding=0, dilation=1, groups=1,
-                 bias=True, padding_mode='zeros', num_experts=3):
+                 bias=True, padding_mode='zeros', num_experts=3, dropout_rate=0.2):
         kernel_size = _pair(kernel_size)
         stride = _pair(stride)
         padding = _pair(padding)
@@ -81,7 +81,7 @@ class CondConv2D(_ConvNd):
             False, _pair(0), groups, bias, padding_mode)
 
         self._avg_pooling = functools.partial(F.adaptive_avg_pool2d, output_size=(1, 1))
-        self._routing_fn = _routing(in_channels, num_experts)
+        self._routing_fn = _routing(in_channels, num_experts, dropout_rate)
         
         self.weight = Parameter(torch.Tensor(
             num_experts, out_channels, in_channels // groups, *kernel_size))
